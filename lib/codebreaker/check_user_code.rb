d2d789d00e938400f5c1295d8b590baf313c6d83
believerfenix <us55554444us55554444@gmail.com>
2021-12-01 10:45:20 +0200

    rename check_usercode -> call, remove map

--- lib/codebreaker/check_user_code.rb
+++ lib/codebreaker/check_user_code.rb
@@ -8,14 +8,16 @@ module Codebreaker
       @user_result = ''
     end
 
-    def check_usercode
+    def call
       strong_match
       soft_match
       @user_result
     end
 
+    private
+
     def strong_match
-      @code_for_check.each_index.map  do |index|
+      @code_for_check.each_index do |index|
         next unless @code_for_check[index] == @guess_code[index]
 
         @user_result += Codebreaker::Constant::STRONG_MATCH
@@ -26,7 +28,7 @@ module Codebreaker
       @code_for_check, @guess_code = remove_double
       return [] if @code_for_check.nil?
 
-      @guess_code.map do |number|
+      @guess_code.each do |number|
         next unless @code_for_check.include?(number)
 
         @code_for_check.delete_at(@code_for_check.index(number))
